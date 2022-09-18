38b3e77a6e5e5b15d7726490ecb8646a5248dfdd
SanjarUsmonov <108736016+SanjarUsmonov@users.noreply.github.com>
2022-09-18 11:22:21 +0500

    gbrt

---
+++ appp.js
@@ -0,0 +1,12 @@
+//#Homework1 JS
+// let number = prompt("Enter number!")
+// let random = parseFloat(number)
+// console.log(Math.round(random * 100) /100 )
+
+
+//#Homework2 JS
+let fam = prompt("name")
+
+
+
+
